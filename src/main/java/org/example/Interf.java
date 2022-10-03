0da5ed269c94a6544d03e770b51197c84e9124e0
AkshathSai <akshathsai.pittala@outlook.com>
2022-10-03 10:22:37 +0530

    Adding O(n) example

---
+++ src/main/java/org/example/Interf.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public interface Interf {
+
+    public void findNemo(String[] array);
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,35 @@
 package org.example;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalTime;
+
 public class Main {
+
+    static String[] nemo = new String[]{"nemo"};
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Interf interf = (array) -> {
+
+            //Instant t0 = Instant.now();
+            LocalTime t0 = LocalTime.now();
+
+            for (int i=0; i<array.length; i++) {
+                if (array[i].equals("nemo")) {
+                    System.out.println("Found Nemo!");
+                }
+            }
+
+            //Instant t1 = Instant.now();
+            LocalTime t1 = LocalTime.now();
+
+            System.out.println("Call to find nemo took " + Duration.between(t0, t1).getNano());
+        };
+
+        interf.findNemo(nemo);
+
     }
+
+
 }
